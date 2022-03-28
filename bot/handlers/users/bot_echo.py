60fe9d2ad02c63a4c5b428b328c93b38a2754248
Turdibek <turdibekdev05@gmail.com>
2022-03-28 14:37:52 +0500

    telegram-moderator

--- bot/handlers/users/bot_echo.py
+++
@@ -1,5 +0,0 @@
-from loader import bot
-
-@bot.message_handler(func=lambda message: True)
-async def echo(message):
-    await bot.send_message(chat_id=message.chat.id, text=message.text)
--- bot/keyboards/inline/privateStartButtons.py
+++ bot/keyboards/inline/privateStartButtons.py
@@ -1,15 +1,20 @@
 from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 buttons = [
-    InlineKeyboardButton(text='Silikon Vodiysi', callback_data='silikon'),
-    InlineKeyboardButton(text='HTML va CSS', callback_data='html_css'),
-    InlineKeyboardButton(text='JavaScript darsliklari',
-                         callback_data='javascript'),
-    InlineKeyboardButton(text='Website yaratish', callback_data='website'),
-    InlineKeyboardButton(text='Live Stream', callback_data='live_stream'),
-    InlineKeyboardButton(text='Sayohat va Vlog', callback_data='sayohat_vlog'),
+    InlineKeyboardButton(text='Silikon Vodiysi',
+                         url="https://youtube.com/playlist?list=PLHNaMQIyrDHOq1tkc_KQXFlLariEonAJR"),
     InlineKeyboardButton(
-        text='API, GitHub, JIRA, HOSTING va boshqalar', callback_data='api'),
+        text='HTML va CSS', url="https://www.youtube.com/playlist?list=PLHNaMQIyrDHOZhHnc2UdQEOHrb1xM7HaF"),
+    InlineKeyboardButton(text='JavaScript darsliklari',
+                         url="https://www.youtube.com/playlist?list=PLHNaMQIyrDHPOSmeUz--1CWj0VW8h26LN"),
+    InlineKeyboardButton(text='Website yaratish',
+                         url="https://youtube.com/playlist?list=PLHNaMQIyrDHM8esvCO8s3ZbY-WVM4-KXT"),
+    InlineKeyboardButton(
+        text='Live Stream', url="https://www.youtube.com/playlist?list=PLHNaMQIyrDHOZhHnc2UdQEOHrb1xM7HaF"),
+    InlineKeyboardButton(text='Sayohat va Vlog',
+                         url="https://youtube.com/playlist?list=PLHNaMQIyrDHN2bVK0_JL8H_QhlI71jaxr"),
+    InlineKeyboardButton(
+        text='API, GitHub, JIRA, HOSTING va boshqalar', url="https://youtube.com/playlist?list=PLHNaMQIyrDHNN6XqrPquiaQwlAemp-dMU"),
 ]
 
 markupStart = InlineKeyboardMarkup(row_width=2).add(*buttons)
